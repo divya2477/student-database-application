c281132f90478700f59da9268e6afe1376beba21
Divya <200502928@student.georgianc.on.ca>
2021-11-04 17:40:38 +0530

    Add project files.

---
+++ studentdatabase/Program.cs
@@ -0,0 +1,14 @@
+﻿using StudentDatabaseManagement.StudentOperations;
+using System;
+
+namespace StudentDatabaseManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations op = new Operations();
+            op.checkForValidLogin();
+        }
+    }
+}
